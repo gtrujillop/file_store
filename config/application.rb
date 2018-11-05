ce2774e7dd25d2ade4234c13f65edf78743cafe3
Gaston Trujillo <gaston.trujillo.java@gmail.com>
2018-11-04 23:13:58 -0500

    Add application setup and gems config files.

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ require "action_view/railtie"
 require "action_cable/engine"
 # require "sprockets/railtie"
 require "rails/test_unit/railtie"
-
+require "active_storage/engine"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -28,6 +28,15 @@ module FileStore
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+    config.log_level = :debug
     config.api_only = true
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post]
+      end
+    end
+    config.active_job.queue_adapter = :sidekiq
+    ActsAsTaggableOn.force_lowercase = true
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'file_uploads#index'
+  resources :file_uploads, only: [:create, :index]
+  get 'file_uploads/:tag_search_query/:page', to: 'file_uploads#index', constraints: { page: /\d/ }
+
 end
